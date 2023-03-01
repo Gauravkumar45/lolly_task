b1c976797b38df514544f8e68c13d34eb30a0336
gaurav kumar <gk4454543@gmail.com>
2023-03-01 22:37:50 +0530

    second commit

--- lolly/lolly_app/views.py
+++ lolly/lolly_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,HttpResponse
 from .models import FileUpload
+from .forms import UploadForm
+
+value = ''
 
 def index(request):
     return render(request,"index.html")
@@ -16,34 +19,49 @@ def checkDuplication(request):
     return render(request,"duplicate.html",params)
     
 def commas(request):
-    if request.method == "POST":
-        num = request.POST.get('text')
-        def format_integer(number, thousand_separator='.'):
-            def reverse(string):
-                string = "".join(reversed(string))
-                return string
-            s = reverse(str(number))
-            count = 0
-            result = ''
-            for char in s:
-                count = count + 1
-                if count % 3 == 0:
-                    if len(s) == count:
-                        result = char + result
-                    else:
-                        result = thousand_separator + char + result
-                else:
+    num = request.POST.get('text','---')
+    def format_integer(number, thousand_separator='.'):
+        def reverse(string):
+            string = "".join(reversed(string))
+            return string
+        s = reverse(str(number))
+        count = 0
+        result = ''
+        for char in s:
+            count = count + 1
+            if count % 3 == 0:
+                if len(s) == count:
                     result = char + result
-            
-            return result
-        print(format_integer(num))
-    return render(request,"commas.html")
+                else:
+                    result = thousand_separator + char + result
+            else:
+                result = char + result
+        
+        return result
+    value = format_integer(num)
+    context = {'value':value}
+        
+    return render(request,"commas.html",context)
 
 def upload(request):
     if request.method == 'POST':
-        files = request.FILES["file"]
-        document = FileUpload.objects.create(file=files)
-        document.save()
-        return HttpResponse('your file was uploaded')
-    return render(request, "upload.html")
+        form = UploadForm(data=request.POST,files=request.FILES)
+        if form.is_valid():
+            form.save()
+            obj = form.instance
+            print(obj)
+            print("===================")
+            return render(request,'upload.html',{'obj':obj})
+    else:
+        form = UploadForm()
+        file = FileUpload.objects.all()
+        print(file)
+        print('---------------------')
+        print(form)
+    return render(request, "upload.html",{"form":form,"file":file})
     
+# def upload(request):
+#     if request.method == 'POST':
+#         files = request.FILES['file'].readlines()
+#         print(files)
+#     return render(request, "upload.html")
